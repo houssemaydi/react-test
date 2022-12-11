02f889cd49ae9499c09c0e0230a7e6df58fa9955
Houssem Aydi <Houssem.aydi@outlook.com>
2022-12-11 22:54:45 +0100

    getByLabeltext

--- src/components/application/application.test.tsx
+++ src/components/application/application.test.tsx
@@ -4,12 +4,39 @@ import { Application } from "./application";
 describe("Application", () => {
 	test("render correctly", () => {
 		render(<Application></Application>);
-		const nameElement = screen.getByRole("textbox");
+
+		const pageHeadingElement = screen.getByRole("heading", {
+			// name: "Job application form",
+			level: 1,
+		});
+		expect(pageHeadingElement).toBeInTheDocument();
+
+		const sectionElement = screen.getByRole("heading", {
+			// name: "Section 1",
+			level: 2,
+		});
+		expect(sectionElement).toBeInTheDocument();
+
+		const nameElement = screen.getByRole("textbox", { name: "Name" });
 		expect(nameElement).toBeInTheDocument();
+
+		const nameElement2 = screen.getByLabelText("Name", { selector: "input" });
+		expect(nameElement2).toBeInTheDocument();
+
+		const termsElement2 = screen.getByLabelText(
+			"I agree to the terms and conditions"
+		);
+		expect(termsElement2).toBeInTheDocument();
+
+		const bioElement = screen.getByRole("textbox", { name: "Bio" });
+		expect(bioElement).toBeInTheDocument();
+
 		const jobLocationElement = screen.getByRole("combobox");
 		expect(jobLocationElement).toBeInTheDocument();
+
 		const termsElement = screen.getByRole("checkbox");
 		expect(termsElement).toBeInTheDocument();
+
 		const submitButtonElement = screen.getByRole("button");
 		expect(submitButtonElement).toBeInTheDocument();
 	});

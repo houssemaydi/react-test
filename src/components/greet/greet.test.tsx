70df209220e2a1f91e06b041aa19ad2b9f419c39
Houssem Aydi <Houssem.aydi@outlook.com>
2022-12-11 19:26:01 +0100

    Try describe inside other

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,14 +1,17 @@
-import { render, screen } from "@testing-library/react"
-import { Greet } from './greet';
+import { render, screen } from "@testing-library/react";
+import { Greet } from "./greet";
 
-test("Greet renders correctly",()=>{
-    render(<Greet/>)
-    const textElement=screen.getByText(/hello/i)
-    expect(textElement).toBeInTheDocument();
-})
-
-test("Greet renders with name props",()=>{
-    render(<Greet name='Houssem'/>)
-    const textElement=screen.getByText("Hello Houssem")
-    expect(textElement).toBeInTheDocument();
-})
+describe("Greet", () => {
+	test(" renders correctly", () => {
+		render(<Greet />);
+		const textElement = screen.getByText(/hello/i);
+		expect(textElement).toBeInTheDocument();
+	});
+	describe("Test", () => {
+		test(" renders with name props", () => {
+			render(<Greet name='Houssem' />);
+			const textElement = screen.getByText("Hello Houssem");
+			expect(textElement).toBeInTheDocument();
+		});
+	});
+});

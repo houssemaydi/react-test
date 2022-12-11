0391f3d493deaed426518415a6c362c5ba27a74e
Houssem Aydi <Houssem.aydi@outlook.com>
2022-12-11 17:26:30 +0100

    add new tests

---
+++ src/components/greet/greet.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react"
+import { Greet } from './greet';
+
+test("Greet renders correctly",()=>{
+    render(<Greet/>)
+    const textElement=screen.getByText(/hello/i)
+    expect(textElement).toBeInTheDocument();
+})
+
+test("Greet renders with name",()=>{
+    render(<Greet name='Houssem'/>)
+    const textElement=screen.getByText("Hello Houssem")
+    expect(textElement).toBeInTheDocument();
+})

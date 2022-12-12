b9b3221e5f6031241dd7281482ca33c403c266a5
Houssem Aydi <Houssem.aydi@outlook.com>
2022-12-12 22:33:01 +0100

    getAllByRole

---
+++ src/components/skills/skills.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import { Skills } from "./Skills";
+
+describe("Skills", () => {
+	const skills = ["HTML", "CSS", "JAVASCRIPT"];
+	test("renders correctly", () => {
+		render(<Skills skills={skills}></Skills>);
+		const listElement = screen.getByRole("list");
+		expect(listElement).toBeInTheDocument();
+	});
+	test("renders a list of skills", () => {
+		render(<Skills skills={skills}></Skills>);
+		const listItemElements = screen.getAllByRole("listitem");
+		expect(listItemElements).toHaveLength(skills.length);
+	});
+});

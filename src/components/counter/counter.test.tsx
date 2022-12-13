91707c03ba0696507ad3dfc69427bb9da758b067
Houssem Aydi <Houssem.aydi@outlook.com>
2022-12-14 00:01:10 +0100

    keyboard interactions

--- src/components/counter/counter.test.tsx
+++ src/components/counter/counter.test.tsx
@@ -40,4 +40,33 @@ describe("Counter", () => {
 		const countElement = screen.getByRole("heading");
 		expect(countElement).toHaveTextContent("2");
 	});
+
+	test("renders a count of 10 after clicking the set button", async () => {
+		user.setup();
+		render(<Counter></Counter>);
+		const amountInput = screen.getByRole("spinbutton");
+		await user.type(amountInput, "10");
+		expect(amountInput).toHaveValue(10);
+		const settButton = screen.getByRole("button", {
+			name: "Set",
+		});
+		await user.click(settButton);
+		const countElement = screen.getByRole("heading");
+
+		expect(countElement).toHaveTextContent("10");
+	});
+
+	test("element are focused in right order", async () => {
+		user.setup();
+		render(<Counter></Counter>);
+		const amountInput = screen.getByRole("spinbutton");
+		const setButton = screen.getByRole("button", { name: "Set" });
+		const incrementButton = screen.getByRole("button", { name: "Increment" });
+		await user.tab();
+		expect(incrementButton).toHaveFocus();
+		await user.tab();
+		expect(amountInput).toHaveFocus();
+		await user.tab();
+		expect(setButton).toHaveFocus();
+	});
 });
